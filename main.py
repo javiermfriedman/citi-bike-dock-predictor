1cb7e7cf2690e6d575b5ac3420e825542b954dc2
javiermfriedman <147084311+javiermfriedman@users.noreply.github.com>
2025-07-24 13:59:34 -0400

    changed hour to string for supabse

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 from database.supabase_client import insert_record
 from data_collection.data_collection import load_data
 from datetime import datetime
-import pytz
 
 def main():
 
     bike_stations_data = load_data()
 
-    ny_tz = pytz.timezone("America/New_York")
-    ny_time = datetime.now(ny_tz)
-    timestamp_str = ny_time.isoformat()
-
     # [station_name, empty_slots, ebikes, temp, description, weekday, hour, month]
     for station_data in bike_stations_data:
 
         payload = {
-                "timestamp": timestamp_str,
                 "station_name": station_data[0],
                 "empty_slots": int(station_data[1]),
                 "ebikes_available": int(station_data[2]),
